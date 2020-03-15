afd526f1c8384d15416f39892ae97f80abb869ac
ilpeppino <ilpeppino@gmail.com>
2020-03-15 15:00:57 +0100

    added simple left-right movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var inputX = Input.GetAxis("Horizontal");
+        var movement = new Vector3(inputX * Time.deltaTime * 10f, 0f, 0f);
+
+        transform.position += movement;
     }
 }
